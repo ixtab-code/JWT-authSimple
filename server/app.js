d1e84ae757f8ebe35e1cba5be456d7d094b58fa2
ixtab <ibana4ik@gmail.com>
2022-11-05 12:54:55 +0300

    add auth router

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRouter = require('./authRouter');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/auth", authRouter);
 
 const start = async () => {
     try {
