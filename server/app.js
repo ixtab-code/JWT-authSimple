02f8def2d7fdfe7bd9c260d34bcec53c206a4c96
ixtab <ibana4ik@gmail.com>
2022-11-05 11:12:15 +0300

    add mogoose connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const start = () => {
+const start = async () => {
     try {
+        await mongoose.connect('mongodb+srv://ixtab:1cluster0.rf2iwla.mongodb.net/?retryWrites=true&w=majority')
         app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
     } catch (err) {
         console.log(err);
