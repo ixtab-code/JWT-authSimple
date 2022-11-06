9a4fe565e97338955eb4c120180f39abc58bfd67
ixtab <ibana4ik@gmail.com>
2022-11-06 08:05:08 +0300

    add registration

--- server/authController.js
+++ server/authController.js
@@ -1,10 +1,21 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 
+const bcrypt = require('bcryptjs');
+
 class authController {
     async registration(req, res) {
         try {
-            
+            const {username, password} = req.body;
+            const candidate = await User.findOne({username})
+            if(candidate) {
+                return res.status(400).json({message: "User already exists", user: candidate})
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({value: 'USER'})
+            const user = new User({username, password: hashPassword, roles: [userRole.value]});
+            await user.save();
+            return res.json({message: "User succesfully saved"});
         } catch (e) {
             console.log(e);
             res.status(400).json({message: 'Regisation error'})
