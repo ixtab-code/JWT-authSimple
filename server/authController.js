390ea1ce815321c09668225474a0340d7444b8dc
ixtab <ibana4ik@gmail.com>
2022-11-06 07:07:54 +0300

    add roles to db

--- server/authController.js
+++ server/authController.js
@@ -1,9 +1,13 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+
 class authController {
     async registration(req, res) {
         try {
             
         } catch (e) {
-
+            console.log(e);
+            res.status(400).json({message: 'Regisation error'})
         }
     }
 
@@ -11,15 +15,21 @@ class authController {
         try {
             
         } catch (e) {
-            
+            console.log(e);
+            res.status(400).json({message: 'Login error'})
         }
     }
 
     async getUsers(req, res) {
         try {
+            /* const userRole = new Role(); */
+            /* const adminRole = new Role({value: 'Admin'}); */
+            /* await userRole.save(); */
+            /* await adminRole.save(); */
             res.json('getUser is working')
         } catch (e) {
-            
+            console.log(e);
+            res.status(400).json({message: 'getUsers error'})
         }
     }
 }
--- server/models/Role.js
+++ server/models/Role.js
@@ -1,6 +1,5 @@
 const {Schema, model} = require('mongoose');
 
-
 const Role = new Schema({
     value: {type: String, unique: true, default: "USER"}
 })
