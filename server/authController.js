ea8dc32727e65bb21f82702861ce5a02dba5606e
ixtab <ibana4ik@gmail.com>
2022-11-08 08:22:30 +0300

    add login and jwt tokenization

--- server/authController.js
+++ server/authController.js
@@ -1,9 +1,19 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 const {validationResult} = require('express-validator')
-
 const bcrypt = require('bcryptjs');
+require('dotenv').config()
+const jwt  = require('jsonwebtoken');
 
+const jwtSecretKey = process.env.jwtSecretKey;
+
+const generateAccessToken = (id, roles) => {
+    const payload = {
+        id, 
+        roles
+    }
+    return jwt.sign(payload, jwtSecretKey, {expiresIn: '24h'})
+}
 class authController {
     async registration(req, res) {
         try {
@@ -30,7 +40,19 @@ class authController {
 
     async login(req, res) {
         try {
-            
+            const {username, password} = req.body;
+            const user = await User.findOne({username})
+            if (!user) {
+                return res.status(400).json({message: `Username ${username} not found`})
+            }
+
+            const validPassword = bcrypt.compareSync(password, user.password)
+
+            if(!validPassword) {
+                return res.status(400).json({message: 'Incorrect password'})   
+            }
+            const token = generateAccessToken(user._id, user.roles);
+            return res.json({token});
         } catch (e) {
             console.log(e);
             res.status(400).json({message: 'Login error'})
