0cadbad9faabda023e9f7f6dfa6fe22ed9c659e3
ixtab <ibana4ik@gmail.com>
2022-11-09 08:15:24 +0300

    add atuh middleware

--- server/authRouter.js
+++ server/authRouter.js
@@ -1,15 +1,15 @@
 const Router = require('express');
 const router = new Router();
+const {check} = require('express-validator') 
 
 const controller = require('./authController');
-
-const {check} = require('express-validator') 
+const authMiddleware = require('./middleware/authMiddleware');
 
 router.post('/registration', [
     check('username', 'Name cannot be empty').notEmpty(),
     check('password', 'Passwords needs to be 4-10 characters long').isLength({min: 4, max: 10}),
 ], controller.registration);
 router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+router.get('/users', authMiddleware, controller.getUsers);
 
 module.exports = router;
